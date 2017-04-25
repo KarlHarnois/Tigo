12e4f6c98147d57451d6dba9bfae0abd16bd57dc
KarlHarnois <karl@goDynamo.com>
2017-04-25 17:36:00 -0400

    First commit

---
+++ Tigo/Tigo.h
@@ -0,0 +1,19 @@
+//
+//  Tigo.h
+//  Tigo
+//
+//  Created by Karl Rivest Harnois on 2017-04-25.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tigo.
+FOUNDATION_EXPORT double TigoVersionNumber;
+
+//! Project version string for Tigo.
+FOUNDATION_EXPORT const unsigned char TigoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tigo/PublicHeader.h>
+
+
